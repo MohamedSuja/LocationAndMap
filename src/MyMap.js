59c2b1abe9cf11142aa6cc107f32c39c135d2165
suja <mohamedsuja123n@gmail.com>
2022-04-13 08:29:18 -0700

    initial commit

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Button, Image} from 'react-native';
+import MapView, {Callout, Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import {PERMISSIONS, RESULTS, check, request} from 'react-native-permissions';
+import {showLocation} from 'react-native-map-link';
 
 const initialState = {
   latitude: 37.78825,
@@ -11,20 +13,68 @@ const initialState = {
 const MyMap = () => {
   const [curentPosition, setCurentPosition] = useState(initialState);
 
-  getMyPosition = () => {
-    Geolocation.getCurrentPosition(info => {
-      console.log(info);
-      setCurentPosition({
-        latitude: info.coords.latitude,
-        longitude: info.coords.longitude,
-      });
+  const getMyPosition = () => {
+    Geolocation.getCurrentPosition(
+      info => {
+        console.log(info);
+        setCurentPosition({
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      e => alert(e.message),
+    );
+  };
+
+  const getPermission = () => {
+    request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+  };
+
+  const checkPermission = fun => {
+    check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then(result => {
+      console.log(result);
+
+      switch (result) {
+        case RESULTS.UNAVAILABLE:
+          alert('This feature is not available on this device');
+          break;
+
+        case RESULTS.DENIED:
+          getPermission();
+          break;
+
+        case RESULTS.GRANTED:
+          console.log('The permission is granted');
+          fun;
+          break;
+
+        case RESULTS.BLOCKED:
+          alert('The permission is denied and not requestable anymore');
+          break;
+      }
     });
   };
+  const test = () => {
+    showLocation({
+      latitude: 7.9824358,
+      longitude: 80.5292226,
+      sourceLatitude: 7.978141,
+      sourceLongitude: 80.526654,
+    });
+  };
+
+  useEffect(() => {
+    return () => {
+      console.log(initialState);
+    };
+  }, [initialState.longitude]);
 
   return (
     <View>
       <MapView
-        style={{height: 700, marginTop: 10}}
+        style={{height: '100%', marginBottom: 0, backgroundColor: '#fff'}}
         initialRegion={{
           latitude: 7.9824358,
           longitude: 80.5292226,
@@ -32,13 +82,39 @@ const MyMap = () => {
           longitudeDelta: 0.0421,
         }}>
         <Marker
+          onDragEnd={e => {
+            setCurentPosition({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+          }}
           coordinate={{
             ...curentPosition,
-          }}
-          image={require('./icon/Location.png')}
-        />
+          }}>
+          <Callout>
+            <Text>My Location</Text>
+          </Callout>
+        </Marker>
       </MapView>
-      <Button title="test" onPress={() => getMyPosition()} />
+      <View
+        style={{
+          position: 'absolute',
+          alignSelf: 'flex-end',
+          bottom: 0,
+          padding: 10,
+        }}>
+        <Button
+          title="MyLocation"
+          onPress={() => checkPermission(getMyPosition())}
+        />
+        <Button
+          title="SetLocation"
+          onPress={() => {
+            console.log(curentPosition);
+            test();
+          }}
+        />
+      </View>
     </View>
   );
 };
